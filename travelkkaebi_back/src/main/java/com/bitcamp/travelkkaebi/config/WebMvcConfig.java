0b2a095bbfb007026f399e65b64e8c536d4c03a0
whdcks420 <whdcks420@gmail.com>
2022-08-25 15:22:51 +0900

    2022.08.25/15pm

--- travelkkaebi_back/src/main/java/com/bitcamp/travelkkaebi/config/WebMvcConfig.java
+++ travelkkaebi_back/src/main/java/com/bitcamp/travelkkaebi/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://192.168.0.13:3000")
+                .allowedOrigins("http://localhost:3000", "http://192.168.0.2:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(MAX_AGE_SECS);
