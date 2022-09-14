1e5afea34f7a74514ae669af8dce37fe3a0e9202
Hwang-cell <kexx1131@gmail.com>
2022-09-14 20:32:56 +0900

    220914 tomato

--- travelkkaebi_front/travelmaker_react/src/pages/Editor/EditorDetail.jsx
+++ travelkkaebi_front/travelmaker_react/src/pages/Editor/EditorDetail.jsx
@@ -67,113 +67,71 @@ function EditorDetail(){
     // let [uymd, uhms] = data.updateTime.split('T');
     // console.log(cymd)
   return (
-    <div>
-
-      <h2 align="center">게시글 상세정보</h2>
+    <div style={{marginTop:"50px"}}>
       <div className="voc-view-wrapper">
-        <div className="voc-view-row">
-            <label>게시글 번호</label>
-            <label>{ data.reviewId }</label>
+        <a href="/review/1" align="left" style={{fontFamily:"'NanumBarunGothic', 'Malgun Gothic', dotum, sans-serif", fontSize:"30px", color:"#548235" , marginBottom:'10px', textDecorationLine : 'none'}}>에디터 추천 게시판</a>
+        <hr style={{backgroundColor : '#548235', height:'3px'}}/>
+        <h2 align="left" style={{fontFamily:"'NanumBarunGothic', 'Malgun Gothic', dotum, sans-serif", fontSize:"20px", marginBottom:'10px'}}>{ data.title }</h2>
+      
+        <div style={{float:"left"}}>
+            {/* <label style={{marginRight:"10px"}}>No. { data.reviewId } </label> */}
+            
+            <label style={{marginRight:"10px"}}>{ data.nickname }</label>
+            <label style={{marginRight:"10px"}}>|</label>
+            <label style={{marginRight:"10px"}}>{ data.region }</label>
+            <label style={{marginRight:"10px"}}>|</label>
+            <label style={{marginRight:"10px"}}>{ data?.createTime?.split('T')[0] }</label>
         </div>
-        <div className="voc-view-row">
-            <label>제목</label>
-            <label>{ data.title }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>작성자</label>
-            <label>{ data.nickname }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>이미지1</label>
-            <label><img src={data.editorImgUrl1} style={{width : "100px", height : "100px"}} /></label>
-        </div>
-        <div className="voc-view-row">
-            <label>이미지2</label>
-            <label><img src={data.editorImgUrl2} style={{width : "100px", height : "100px"}} /></label>
-        </div>
-        <div className="voc-view-row">
-            <label>이미지3</label>
-            <label><img src={data.editorImgUrl3} style={{width : "100px", height : "100px"}} /></label>
+        <div style={{float:"right"}}>
+          <label align="right" style={{marginRight:"10px"}}>조회수 : { data.view }</label>
         </div>
 
-        <div className="voc-view-row">
-            <label>조회수</label>
-            <label>{ data.view }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>작성일</label>
-            <label>{ data.createTime }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>수정일</label>
-            <label>{ data.updateTime }</label>
-        </div>
-        <div className="voc-view-row">
-            <label>내용</label>
-            <div>
-                {
-                data.content
-                }
-            </div>
-        </div>
-        <div style={{margin: "10px 0", display: "flex"}}>
-              <button type='button' className='btn btn-info'
+        <br></br>
+        <hr/>
+        <div style={{float:"right"}}>
+          <div  style={{margin: "10px 0", display: "flex"}}>
+              <button  type='button' className='btn btn-info'
               style={{width:'100px', marginRight:'10px'}}
               onClick={()=>{
-                navi("/review/form");
+                navi("/editor/form");
               }}>수정</button>
 
               <button type='button' className='btn btn-info'
               style={{width:'100px', marginRight:'10px'}}
               onClick={()=>{
-                navi("/review/form");
+                onDelete();
               }}>삭제</button>
 
-              <button type='button' className='btn btn-success'
+              <button  type='button' className='btn btn-success'
               style={{width:'100px', marginRight:'10px'}}
               onClick={()=>{
-                navi(`/review/1`);
+                navi(`/editor/1`);
               }}>목록</button>
+          </div>
         </div>
+
+
         <div>
-    </div>
-        <div className="voc-view-reply" style={{marginTop:"100px"}}>
-            <label style={{height:"100%", margin:"auto"}}>댓글</label>
-            <label>
-            <div>
-                {
-                reply && reply.map((row, idx)=>(
-                  <tr>
-                    <td key={row.reviewReplyId}>{row.nickname}</td>
-                  
-                  </tr>
-                ))
-                }
-            </div>
-            </label>
-            <label>
-            {
-                reply && reply.map((row, idx)=>(
-                  <tr>
-                    <td key={row.reviewReplyId}>{row.comment}</td>
-                  </tr>
-                ))
-            }
-            </label>
-            <label>
-            {
-                reply && reply.map((row, idx)=>(
-                  <tr>
-                    <td key={row.reviewReplyId}>{row.comment}</td>
-                  </tr>
-                ))
-            }
-            </label>
+            <label><img src={data.profileImageUrl} style={{width : "80px", height : "80px", borderRadius:"50%" }} /></label>
         </div>
+
+        <div style={{marginTop:"50px"}}>
+            <label align="center"><img src={data.editorImgUrl1} style={{width : "100%"}} /></label>
+            <label align="center"><img src={data.editorImgUrl2} style={{width : "100%"}} /></label>
+            <label align="center"><img src={data.editorImgUrl3} style={{width : "100%"}} /></label>
+        </div>
+        
+        <div style={{marginTop:"50px", marginBottom:"150px"}}>
+          {
+          data.content
+          }
+        </div>
+      </div>
+
 
 
       </div>
-    </div>
+    
     
   )
 
---
+++ travelkkaebi_front/travelmaker_react/src/pages/Editor/EditorUpdateForm.jsx
@@ -0,0 +1,321 @@
+import { ContainerWrapper, FormTitle, Title, Wrapper } from "./EditorCreatestyle";
+import "./RegionEventCreateForm.css";
+import axios from 'axios';
+import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Button, IconButton } from "@mui/material";
+import Logo from ".//basicLogo.png";
+import { PhotoCamera } from "@mui/icons-material";
+import React, { useMemo, useRef, useState } from "react";
+import { API_BASE_URL } from '../../config';
+import { useForm } from "react-hook-form";
+import { bearerToken, headerConfig, headerImg_tk } from "../../util";
+
+const EditorUpdateForm = () => {
+  const [photo, setPhoto] = useState('');
+  const [subject, setSubject] = useState('');
+  const [content, setContent] = useState('');
+
+
+  const imageUpload = (e) => {
+    e.preventDefault();
+    if (e.target.files[0]) {
+      // 새로운 이미지를 올리면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+      URL.revokeObjectURL(profile.preview_URL);
+      const preview_URL = URL.createObjectURL(e.target.files[0]);
+      setProfile(() => ({
+        image_file: e.target.files[0],
+        preview_URL: preview_URL,
+      }));
+    }
+  };
+
+  // loginStatus 추후에 맞춰서 변경
+  const navi = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    watch,
+    getValues,
+    formState: { isSubmitting, isDirty, errors },
+  } = useForm({
+    mode: "onChange",
+  });
+  //let loginStatus = localStorage.loginStatus;
+  //loginStatus = 1;  로그인 상태 임시로 켜둔거
+  // let id = localStorage.myid; //localStorage.myid;
+
+  const logInStatus=()=>{
+    if(localStorage.username==null){
+      alert("먼저 로그인한 후 글을 작성해주세요");
+      navi("/login");
+    }
+  }
+  
+  // let reviewId =1;
+  // let categoryId =1;
+  // let userId="userIdT";
+  // let title="titleT";
+  // let region ="서울";
+  // let view =0;
+  // let createTime="2022.06.06";
+  // let updateTime="2022.06.06";
+
+
+  // profile 이미지 상태
+  const [profile, setProfile] = useState({
+    image_file: "",
+    preview_URL: Logo,
+  });
+  let inputRef;
+
+  const onSubmit= async (data)=>{
+    console.log("check");
+    const headerConfig = {
+      Headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+    // data.preventDefault();
+    console.log(data);
+    
+    const formData = new FormData();
+    console.log(formData);
+    const EditorChoiceDTO = JSON.stringify(data);
+    // EditorChoiceDTO.id =1;
+    formData.append("file", profile.image_file);
+    formData.append(
+      "EditorChoiceDTO",
+      new Blob([EditorChoiceDTO], { type: "application/json" })
+    );
+    console.log(formData);
+
+    axios.defaults.headers = {
+      "Content-Type": "application/json; charset = utf-8",
+      Authorization: "Bearer " + localStorage.getItem("ACCESS_TOKEN"),
+    };
+    await axios
+    .post(API_BASE_URL + "/editorchoice/write", formData, headerConfig)
+    .then((res) => {
+      console.log(res.data);
+      alert("👹에디터 글 작성이 완료되었습니다.");
+      navi('/editor');
+    });
+
+  }
+
+  useEffect(()=>{
+      // 로그인 체크 함수
+      logInStatus();
+    },[]);
+
+  return (
+    <div>
+{/* 에디터 글쓰기 DTO 필요한것
+title, content, region, img url 1~3
+*/}
+<Wrapper>
+      <ContainerWrapper>
+        <form
+          className="reg_form"
+          id="reg_form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="register_form">
+            <FormTitle>
+              에디터 글쓰기
+              <p className="must">필수입력사항 </p>
+            </FormTitle>
+            <br />
+            <div className="reg_table" style={{ margin: 0, display: "block" }}>
+              <table className="register_table">
+                <colgroup style={{ display: "table-column-group" }}>
+                  <col style={{ width: 130, display: "table-column" }} />
+                  <col style={{ width: "*", display: "table-column" }} />
+                </colgroup>
+                <tbody>
+                  <tr>
+                    <td scope="row">
+                      <label htmlFor="photo" className="req">
+                        👹사진 첨부 부분
+                      </label>
+                    </td>
+
+                    
+                    <div className="profileimg">
+                    
+              <img
+                alt="basicimg"
+                src={profile.preview_URL}
+                className="user_profile"
+              />
+            </div>
+            <div className="photo_icon">
+              <input
+                type="file"
+                accept="image/*"
+                name="profile_img_url"
+                hidden
+                style={{ display: "none" }}
+                onChange={imageUpload}
+                ref={(refParam) => (inputRef = refParam)}
+                onClick={(e) => (e.target.value = null)}
+              />
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                onClick={() => inputRef.click()}
+              >
+                <PhotoCamera />
+              </IconButton>
+            </div>
+            <br />
+
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <label htmlFor="title" className="req">
+                        🔸제목
+                      </label>
+                    </th>
+                    <td>
+                      <input
+                        className="reg_input"
+                        type="text"
+                        name="title"
+                        id="title"
+                        required
+                        autoComplete="off"
+                        aria-invalid={
+                          !isDirty
+                            ? undefined
+                            : errors.nickname
+                            ? "true"
+                            : " false"
+                        }
+                        {...register("title", {
+                          maxLength: {
+                            value: 30,
+                            message: "30글자까지 입력 가능합니다.",
+                          },
+                          minLength: {
+                            value: 5,
+                            message: "5글자 이상 입력 가능합니다.",
+                          },
+                        })}
+                      />
+
+                      
+                    </td>
+                  </tr>
+
+                  
+                  <tr>
+                    <th scope="row">
+                      <label htmlFor="content" className="req">
+                        🔸내용
+                      </label>
+                    </th>
+                    <td>
+                      <div className="content_wrap">
+                        <input
+                          type="content"
+                          className="reg_input"
+                          name="content"
+                          required
+                          
+                          {...register("content", {
+                            maxLength: {
+                              value: 500,
+                              message: "최대 500글자까지 입력 가능합니다.",
+                            },
+                            
+                          })}
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <BtnConfirm>
+              <a href="/regionevent" className="btn_cancel">
+                취소
+              </a>
+              <input
+                type="submit"
+                disabled={isSubmitting}
+                value="글 작성"
+                id="btn_submit"
+                className="btn_submit"
+                accessKey="s"
+              />
+            </BtnConfirm>
+          </div>
+        </form>
+      </ContainerWrapper>
+    </Wrapper>
+
+      {/* <img alt='' src={photoUrl+photo} className='imgphoto'/> */}
+      {/* <form onSubmit={onBoardInsert}>
+        <table className='table table-bordered' style={{width:'400px'}}>
+          <caption><h3>지역축제게시판 글쓰기</h3></caption>
+          <tbody>
+            <tr>
+              <th style={{backgroundColor:'#ddd'}} width='100'>아이디</th>
+              <td>{id}</td>
+            </tr>
+            <tr>
+              <th style={{backgroundColor:'#ddd'}} width='100'>대표 이미지</th>
+              <td>
+                <input type='file' className='form-control'll
+                style={{width:'250px'}} 
+                onChange={imageUpload} required/>
+              </td>
+            </tr>
+            <tr>
+              <th style={{backgroundColor:'#ddd'}} width='100'>제목</th>
+              <td>
+                <input type={'text'} className="form-control"
+                style={{width:'300px'}} required
+                onChange={(e)=>{
+                  setSubject(e.target.value);
+                }}/>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                <textarea name="textarea" className='form-control' required
+                style={{width:'400px', height:'120px'}}
+                onChange={(e)=>{
+                  setContent(e.target.value);
+                }}></textarea>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} align='center'>
+                <button type="submit" className='btn btn-info'>게시글 저장</button>
+                <button type="button" className='btn btn-success'
+                style={{marginLeft:'10px'}}
+                onClick={()=>{
+                  navi("/regionevent");
+                }}>게시판 메인</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form> */}
+    </div>
+  );
+};
+
+export default EditorUpdateForm;
+
+const BtnConfirm = styled.div`
+  text-align: "center";
+  margin: 55px auto 0 !important;
+  display: block;
+`;
